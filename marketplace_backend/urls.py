430113644bf7dba02f5d03145a450ebd83c28844
syedtayab321 <tayabsajid5@gmail.com>
2025-02-01 11:19:33 +0500

    urls files added

--- marketplace_backend/urls.py
+++ marketplace_backend/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authApis/', include('authentication.urls')),
+    path('logisticsDeliveryApis/', include('logistics_delivery.urls')),
+    path('ordersApis/', include('orders.urls')),
+    path('paymentApis/', include('payment.urls')),
+    path('listingsApis/', include('product_listings.urls')),
+    path('profileApis/', include('profile_management.urls')),
+    path('searchFiltersApis/', include('search_filters.urls')),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
